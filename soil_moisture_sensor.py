ca6a8224f37c1dea579b8bd9a13af4fbbe68fe94
HariNarayana123 <62258694+HariNarayana123@users.noreply.github.com>
2023-03-30 19:35:00 +0530

    Update soil_moisture_sensor.py

--- soil_moisture_sensor.py
+++ soil_moisture_sensor.py
@@ -9,10 +9,11 @@ vref=3.3v
 def read_adc(channel):
       adc =spi.xfer2([1,(8+channel)<<4,0])
       data=((adc[1] & ob11) <<8) + adc[2])
-       voltage= (adc_value * vref) / 1023.0
-       return round(voltage,2)
+      voltage= (adc_value * vref) / 1023.0
+      return round(voltage,2)
      
 while True:
      res=read_adc(channel)
+     print(" voltage: ", voltage)
      time.sleep(0.5)
 spi.close()
