4433fadd0ca55a0acc9893493956b35f3436b14d
HariNarayana123 <62258694+HariNarayana123@users.noreply.github.com>
2023-03-30 16:43:34 +0530

    Source code soil_misture_sensor.py

---
+++ soil_moisture_sensor.py
@@ -0,0 +1,18 @@
+Import spidev
+Import time
+Spi=spidev.SpiDev()
+Spi.open(0,0)
+
+channel=0
+vref=3.3v
+
+def read_adc(channel):
+      adc =spi.xfer2([1,(8+channel)<<4,0])
+      data=((adc[1] & ob11) <<8) + adc[2])
+       voltage= (adc_value * vref) / 1023.0
+       return round(voltage,2)
+     
+while True:
+     res=read_adc(channel)
+     time.sleep(0.5)
+spi.close()
